9f4acfb7ad913c71e3e1e37a9abea30a2e593157
Brandon-Consulting <Brandon@TheAIConsultingFirm.com>
2024-02-13 10:46:02 -0700

    Added TACF Logo, redirected header to TACF Logo, Changed Size of Header up by x2, Changed Size of Chat Logo by x2, Reduced Top Margin for Chat Title from 36 to 24

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet, Link } from "react-router-dom";
 import styles from "./Layout.module.css";
-import BWLogo from "../../assets/BWLogo.svg";
+import TACFLogo from "../../assets/TACFLogo.pdf";
 import { CopyRegular, ShareRegular } from "@fluentui/react-icons";
 import { Dialog, Stack, TextField, ICommandBarStyles, IButtonStyles } from "@fluentui/react";
 import { useContext, useEffect, useState } from "react";
@@ -47,12 +47,12 @@ const Layout = () => {
                 <Stack horizontal verticalAlign="center" horizontalAlign="space-between">
                     <Stack horizontal verticalAlign="center">
                         <img
-                            src={BWLogo}
+                            src={TACFLogo}
                             className={styles.headerIcon}
                             aria-hidden="true"
                         />
                         <Link to="/" className={styles.headerTitleContainer}>
-                            <h1 className={styles.headerTitle}>powered by The AI Consulting Firm</h1>
+                            <h1 className={styles.headerTitle}>Powered by The AI Consulting Firm</h1>
                         </Link>
                     </Stack>
                     <Stack horizontal tokens={{ childrenGap: 4 }}>
